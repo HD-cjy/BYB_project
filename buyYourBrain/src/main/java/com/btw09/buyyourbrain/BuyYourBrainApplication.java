38c2f3549df9dad62bce5a7e5e5254e85fcd3c0a
HD-cjy <hdcjy5142@gmail.com>
2025-07-10 10:41:55 +0900

    git init files

---
+++ buyYourBrain/src/main/java/com/btw09/buyyourbrain/BuyYourBrainApplication.java
@@ -0,0 +1,13 @@
+package com.btw09.buyyourbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyYourBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyYourBrainApplication.class, args);
+	}
+
+}
