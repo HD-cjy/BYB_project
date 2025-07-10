dfb6af1fa6822b43882263959f941c1d4dad8376
HD-cjy <hdcjy5142@gmail.com>
2025-07-10 15:04:02 +0900

    init security

--- buyYourBrain/src/main/java/com/btw09/buyyourbrain/BuyYourBrainApplication.java
+++ buyYourBrain/src/main/java/com/btw09/buyyourbrain/BuyYourBrainApplication.java
@@ -2,6 +2,7 @@ package com.btw09.buyyourbrain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BuyYourBrainApplication {
@@ -9,5 +10,8 @@ public class BuyYourBrainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BuyYourBrainApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String index() {
+	    return "redirect:/boot/"; // 혹은 /boot/home 등
+	}
 }
